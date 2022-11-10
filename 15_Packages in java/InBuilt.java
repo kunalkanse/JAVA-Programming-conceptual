778e0e4388274e591cfdf683ebae321d6b77ad24
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-11-10 12:06:36 +0530

    Create InBuilt.java

---
+++ 15_Packages in java/InBuilt.java
@@ -0,0 +1,23 @@
+
+// inbuilt packages
+
+import java.io.*;
+//import java.util.*;
+import java.util.Scanner;
+import java.net.*;
+import java.awt.*;
+import java.sql.*;
+import javax.swing.*;
+
+class InBuilt
+{
+	public static void main(String args[])
+	{
+		Scanner sobj = new Scanner(System.in);
+		
+		System.out.println("Enter the number : ");
+		int no = sobj.nextInt();
+		
+		System.out.println(no);
+	}
+}
