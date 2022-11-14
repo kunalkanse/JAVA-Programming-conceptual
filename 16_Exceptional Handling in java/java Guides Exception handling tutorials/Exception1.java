c1e0fba16f59309cb33eeac6e7503faf930c1e87
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-11-14 22:52:39 +0530

    Add files via upload

---
+++ 16_Exceptional Handling in java/java Guides Exception handling tutorials/Exception1.java
@@ -0,0 +1,25 @@
+
+/*
+
+try
+{
+    // block of code to monitor for errors
+
+}
+
+catch
+{
+    // exception handler for ExceptionType1
+}
+
+catch
+{
+    // exception handler for ExceptionType2
+}
+
+finally
+{
+// block of code to be executed after try block ends
+}
+
+*/
