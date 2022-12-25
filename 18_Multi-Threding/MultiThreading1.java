9a57da7b60b2c43bcd756ea6b90649967575c684
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-12-25 23:38:26 +0530

    Create MultiThreading1.java

---
+++ 18_Multi-Threding/MultiThreading1.java
@@ -0,0 +1,35 @@
+
+
+// creating thread using Runnable interface
+class MyThread1 implements Runnable
+{
+    public void run()
+    {
+        for(int i = 0; i < 10 ; i++)
+        {
+            System.out.println("i = "+i);
+            
+            try
+            {
+                Thread.sleep(1000);     // Checked Exception
+            }
+            catch(Exception e)
+            {
+                
+            }
+        }
+    }
+
+    public static void main(String args[])
+    {
+        MyThread1 mythread = new MyThread1();
+
+        MyThread2 t2 = new MyThread2(); // this class is in another file
+
+        Thread t1 = new Thread(mythread);
+
+        t1.start();
+        t2.start();
+
+    }
+}
