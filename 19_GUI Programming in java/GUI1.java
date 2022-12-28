7d28f658ce300c3cab406516a82c75f5e0007820
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-12-28 22:31:23 +0530

    Create GUI1.java

---
+++ 19_GUI Programming in java/GUI1.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+
+/**
+ * This program demonstarte how to create window using AWT libraries
+ * @author Kunal kanse
+ */
+class GUI1
+{
+    public static void main(String[] args) {
+        
+        // Frame is a class
+        Frame fobj = new Frame("Kunal's First Window");
+
+        fobj.setSize(500, 500);
+        fobj.setVisible(true);
+
+        // to close the window press => ctrl + c 
+    }
+}
