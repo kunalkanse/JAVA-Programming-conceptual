2318a7353fc9b65b05d42a811957e66be871f339
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2023-03-08 17:40:53 +0530

    Update MultiThreading1.java

--- 18_Multi-Threding/MultiThreading1.java
+++ 18_Multi-Threding/MultiThreading1.java
@@ -1,4 +1,32 @@
 
+// creating thread using Runnable interface
+class MyThread2 extends Thread
+{
+
+    public MyThread2(String s)
+    {
+        super(s);   // calling constructor : Thread(String s)
+    }
+    
+    public void run()
+    {
+        for(int i = 0; i < 10 ; i++)
+        {
+            System.out.println("i = "+i);
+            
+            try
+            {
+                Thread.sleep(1000);     // Checked Exception
+                //sleep() method throws InterruptedException exception, when another thread interrupts the current thread while sleep is active.
+            }
+            catch(InterruptedException ie)
+            {
+                
+            }
+        }
+    }
+}
+
 
 // creating thread using Runnable interface
 class MyThread1 implements Runnable
